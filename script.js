398564b9c543016dd9309f5d97e5e9cfb6800f2a
Mixtyc1 <victor.martins56763@gmail.com>
2025-08-02 04:18:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 const envelope = document.getElementById("envelope");
 const flap = document.querySelector(".flap");
 const carta = document.getElementById("carta");
+const selo = document.querySelector(".selo");
 
-envelope.addEventListener("click", () => {
-  flap.style.transform = "rotateX(-180deg)";
-  setTimeout(() => {
-    carta.style.display = "block";
-  }, 1000);
+selo.addEventListener("click", (e) => {
+  e.stopPropagation();
+  envelope.classList.add("aberto");
 });
